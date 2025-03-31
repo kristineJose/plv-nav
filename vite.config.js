f458723cf5719c4ab9d88e3c0df7b47bcdb60f91
kristineJose <kristin.jose24@gmail.com>
2025-03-31 14:46:57 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     assetsInclude: ["**/*.gltf"],
-    base: "/ARIndoorNavigation-Threejs/",
+    base: "/plv-nav/",
     publicDir: "static/",
     server: {
         host: true,
-    },
+    },  
     plugins: [basicSsl()],
 });
